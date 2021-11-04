7db920b2aadf0595ccc00156eacf0a8bfebb0cf1
Bhotton25 <bhotton25@gmail.com>
2021-11-03 20:28:12 -0600

    Added enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,11 +5,11 @@ var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
   window.alert("Welcome to Robot Gladiators!");
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   if(promptFight === 'fight' || promptFight === "FIGHT"){
@@ -49,4 +49,6 @@ else{
     window.alert("You need to choose a valid option. Try again!");
 }
 };
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
